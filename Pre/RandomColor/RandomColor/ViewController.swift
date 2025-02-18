d59eb436a7e094c9c3d4b1dd69708677968e4841
KwakDADA <down022@gmail.com>
2025-02-18 17:31:13 +0900

    1-2. UILabel

--- Pre/RandomColor/RandomColor/ViewController.swift
+++ Pre/RandomColor/RandomColor/ViewController.swift
@@ -15,18 +15,31 @@ class ViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+    
+    private let colorInfoLabel: UILabel = {
+       let label = UILabel()
+        label.text = "R: 255, G: 255, B: 255"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUI()
+        setAddView()
         setConstraints()
     }
     
     // MARK: - Set UI
     private func setUI() {
         view.backgroundColor = .white
+    }
+    
+    // MARK: - Set Add View
+    private func setAddView() {
         view.addSubview(backgroundView)
+        backgroundView.addSubview(colorInfoLabel)
     }
     
     // MARK: - Set Constraints
@@ -37,6 +50,12 @@ class ViewController: UIViewController {
             backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        // 색상 정보 라벨
+        NSLayoutConstraint.activate([
+            colorInfoLabel.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor),
+            colorInfoLabel.centerYAnchor.constraint(equalTo: backgroundView.centerYAnchor)
         ])
     }
 
